3ba924894fde559ac2d89e5ed3cc1cb83953d957
ymazure <ymazure.dev@gmail.com>
2022-05-26 20:52:41 +0300

    added console app

---
+++ Other/OtherStuff/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+class A
+{
+  const string name = "qwe";
+  const int I = 1;
+  const B b = new B();
+  const C c = default(C);
+}
+
+class B
+{
+  public string Name;
+}
+
+struct C
+{
+  public int Y = 1;
+}
